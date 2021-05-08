43e3286b48d777b0dbbf243bc9ab005e51125d38
Dell <muskan.mittal_cs18@gla.ac.in>
2021-05-08 18:50:37 +0530

    java pattern

---
+++ src/pattern_1/Pattern_1.java
@@ -0,0 +1,14 @@
+package pattern_1;
+
+public class Pattern_1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=0; i<5; i++){
+            for(int j=i; j<5; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
